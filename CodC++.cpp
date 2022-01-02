8b44f976cc336a9ef7045a8d5f5639c07138c22d
Vamanu Petru <petrisor_vmy@yahoo.com>
2022-01-02 19:15:43 +0200

    while()

--- CodC++.cpp
+++ CodC++.cpp
@@ -1,24 +1,13 @@
 #include<iostream>
 using namespace std;
-
+double i;
 int main()
 {
-      cin >> nrA;
-      cin >> nrB;
-      if(nrA>nrB)
+      i = 4;
+      cout << i << '\n';
+      i = i - 1;
+      while(i > 0)
       {
-            cout << nrA << '\n';
-            i = 213;
-            if(i > 200)
-            {
-                  cout << "DA" << '\n';
-            }
-            else{
-                  cout << "NU" << '\n';
-            }
       }
-      else{
-            cout << nrB << '\n';
-      }
-   return 0;
+      return 0;
 }
